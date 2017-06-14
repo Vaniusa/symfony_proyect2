6add95a5565827321bc3a0de13c4387d0cbeed61
Vaniusa <ionsaitan1980@gmail.com>
2017-06-14 18:07:33 +0200

    4 commit v31

--- src/Vaniusa/BlogBundle/Controller/DefaultController.php
+++ src/Vaniusa/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Vaniusa\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('VaniusaBlogBundle:Default:index.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('VaniusaBlogBundle:Post')->findAll();
+        return $this->render('VaniusaBlogBundle:Default:index.html.twig', array('post'=>$post));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ppost = $em->getRepository('VaniusaBlogBundle:Post')->find($id);
+        return $this->render('VaniusaBlogBundle:Default:show.html.twig', array('ppost'=>$ppost));
+    }
+
+    public function langAction(Request $request)
+    {
+        return $this->redirectToRoute("mi_homepage");
     }
 }
